b4c6816d3f9dea346398b2d22358378a922c797f
Jonas Heinle <jonasheinle@googlemail.com>
2019-04-11 11:32:55 +0200

    begin to track the development of my website

---
+++ jotrockenmitlocken.de/startFrames/Forum/vorschlagEingabe.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+	    <title>
+		    "Esse quam videri"
+		</title>
+	</head>
+	<body>
+	    <?php
+		    echo "Danke für deinen Vorschlag " . $_POST[vor] . " " . "Wir sind bemüht und kümmern uns um ihren Vorschlag<br />";
+			if ($_POST["vor"] == "Jonas") echo "<br />FunFact: Du hast den selben Vornamen wie der Autor dieser Seite" 
+		?>
+	</body>
+</html>
